fe9359b803f9a65111cbacda3b3591e5da98bf89
Caio-Marinho <kaigabril12@gmail.com>
2026-02-15 19:50:54 -0300

    Remove export_requirements.py do repositório

--- export_requirements.py
+++
@@ -1,8 +0,0 @@
-import pkg_resources
-
-ignore = {"pyngrok"}  # pacotes que você quer ignorar
-
-with open("requirements.txt", "w") as f:
-    for dist in pkg_resources.working_set:
-        if dist.project_name not in ignore:
-            f.write(f"{dist.project_name}=={dist.version}\n")
